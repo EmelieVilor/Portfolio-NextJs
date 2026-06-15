48cbe4dcb68924a4f09fa56e020fe8fdf80da91d
Emelie Nilsson Vilör <emelie.vilor@gmail.com>
2026-06-15 12:48:44 +0200

    snyggade till formulär

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -11,9 +11,14 @@ export async function POST(req: Request) {
   await resend.emails.send({
     from: "onboarding@resend.dev",
     to: "emelie.vilor@gmail.com",
-    subject: `New message from ${name}`,
+    subject: `Portfolio message from ${name}`,
     text: `From: ${email}\n\n${message}`,
-    // html: '<p className="text-red">Message sent!</p>',
+    html: `<div style="font-family: sans-serif; margin: auto; padding: 24px;">
+      <h2 style="font-size: 20px;">New message from: ${name}</h2>
+      <p><strong>From:</strong> ${email}</p>
+      <hr style="border: none; border-top: 1px solid #eee;" />
+      <p style="font-size: 16px; line-height: 1.6;">${message}</p>
+    </div>`,
   });
 
   return NextResponse.json({ success: true });
--- components/ui/EmailForm.tsx
+++ components/ui/EmailForm.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelopeCircleCheck } from "@fortawesome/free-solid-svg-icons";
 
 export default function EmailForm() {
-  const [form, setForm] = useState({ name: "", email: "", message: "", honeypot: ""});
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+    honeypot: "",
+  });
   const [sent, setSent] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -21,30 +26,43 @@ export default function EmailForm() {
 
   return (
     <div className="EmailForm mt-5">
-      <form className="flex flex-col gap-4 w-full lg:w-110" onSubmit={handleSubmit}>
+      <form
+        className="flex flex-col gap-4 w-full lg:w-110"
+        onSubmit={handleSubmit}
+      >
         <div>
           <p>Name</p>
+          {/* value för att kunna hantera varje input med setForm */}
           <input
+            value={form.name}
             className="bg-dark border-2 border-light mt-2 px-3 py-2 w-full text-[14px] font-mono tracking-wide"
-            onChange={(e) => setForm({ ...form, name: e.target.value })} required
+            onChange={(e) => setForm({ ...form, name: e.target.value })}
+            required
           ></input>
         </div>
         <div>
           <p>E-mail</p>
           <input
+            value={form.email}
             className="bg-dark border-2 border-light mt-2 px-3 py-2 w-full text-[14px] font-mono tracking-wide"
-            onChange={(e) => setForm({ ...form, email: e.target.value })} required
+            onChange={(e) => setForm({ ...form, email: e.target.value })}
+            required
           ></input>
         </div>
         <div>
           <p>Message</p>
-        <textarea
-          className="bg-dark w-full border-2 border-light mt-2 px-3 py-2 h-40 text-[14px] font-mono tracking-wide"
-          onChange={(e) => setForm({ ...form, message: e.target.value })}
-          placeholder="Say hello..."
-        ></textarea>
+          <textarea
+            value={form.message}
+            className="bg-dark w-full border-2 border-light mt-2 px-3 py-2 h-40 text-[14px] font-mono tracking-wide"
+            onChange={(e) => setForm({ ...form, message: e.target.value })}
+            placeholder="Say hello..."
+          ></textarea>
         </div>
-        <input className="honeypot hidden" onChange={(e) => setForm({ ...form, honeypot: e.target.value })}></input>
+        <input
+          value={form.honeypot}
+          className="honeypot hidden"
+          onChange={(e) => setForm({ ...form, honeypot: e.target.value })}
+        ></input>
         <button
           className="flex gap-3 w-full items-center justify-center bg-accent uppercase text-xs tracking-widest font-sans py-3 px-5 border-2 border-accent hover:text-dark transition-all"
           type="submit"
@@ -52,9 +70,11 @@ export default function EmailForm() {
           Send Message
         </button>
 
-      {sent && (
-        <p className="text-light text-center">Message sent! <FontAwesomeIcon icon={faEnvelopeCircleCheck}/></p>
-      )}
+        {sent && (
+          <p className="text-light text-center">
+            Message sent! <FontAwesomeIcon icon={faEnvelopeCircleCheck} />
+          </p>
+        )}
       </form>
     </div>
   );
