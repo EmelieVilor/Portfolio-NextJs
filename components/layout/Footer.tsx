3e7fd51436b8632884fe085bd056d0c7f9da543e
Emelie Nilsson Vilör <emelie.vilor@gmail.com>
2026-06-15 09:55:46 +0200

    adjusted date in footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+'use client'
 import '@fortawesome/fontawesome-svg-core/styles.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,12 +6,11 @@ import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer() {
-
-const today = new Date();
+const todaysDate = new Date().toLocaleDateString('sv-SE');
 
   return (
     <div className="Footer p-10 text-center flex flex-col gap-1 bg-lighter pt-25 mt-20" style={{ clipPath: "polygon(0 0, 100% 35%, 100% 100%, 0 100%)" }}>
-        <p>{today.toLocaleDateString()} © Emelie Nilsson Vilör</p>
+        <p>{todaysDate} © Emelie Nilsson Vilör</p>
         <p className="text-accent">Created using Next.js | Tailwind CSS | Adobe Illustrator</p>
           <div className="flex gap-2 justify-center items-center mt-4">
           <a href="https://github.com/EmelieVilor"><FontAwesomeIcon className="text-lg hover:scale-110 transition-all" icon={faGithub} /></a>
